6ad39eacffebb6612ea21172bfbcc539bf765f15
Comb1e <101026561+Comb1e@users.noreply.github.com>
2025-02-05 21:44:12 +0800

    删了没用到的

--- component/algorithm/crc.c
+++ component/algorithm/crc.c
@@ -6,16 +6,20 @@
 
 static uint16_t U2(uint8_t *p) {uint16_t u;memcpy(&u, p, 2);return u;}
 
-void crc16_update(uint16_t *currect_crc, const uint8_t *src, uint32_t len) {
+void crc16_update(uint16_t *currect_crc, const uint8_t *src, uint32_t len)
+{
     uint32_t crc = *currect_crc;
     uint32_t j;
-    for (j = 0; j < len; ++j) {
+    for (j = 0; j < len; ++j)
+    {
         uint32_t i;
         uint32_t byte = src[j];
         crc ^= byte << 8;
-        for (i = 0; i < 8; ++i) {
+        for (i = 0; i < 8; ++i)
+        {
             uint32_t temp = crc << 1;
-            if (crc & 0x8000) {
+            if (crc & 0x8000)
+            {
                 temp ^= 0x1021;
             }
             crc = temp;
